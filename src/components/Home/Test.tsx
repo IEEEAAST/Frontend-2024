a401aa460dba32a0a85d0628b6a81f0719806a43
Ali-Aref1 <ali14915@gmail.com>
2024-04-12 18:19:15 +0200

    Messed around with the homepage (nothing useful)

--- src/components/Home/Test.tsx
+++ src/components/Home/Test.tsx
@@ -1,9 +1,11 @@
-import Logo from '../../assets/IEEELogoBlue.png'
+import Logo from '../../assets/IEEELogoWhite.png'
 import './styles/Test.css'
 export const Test = () => {
   return (
     <div id="flex">
-    <img id="testimg" src={Logo}></img>
+      <div id="circle"><img id="testimg" src={Logo}></img></div>
+      This is our test page; To be replaced with the actual home page.<br></br>
+      For now, enjoy this spinning IEEE thing.
     </div>
   )
 }
