bf7641a3ae535ebb47b87b5ed7f1dd81246fe06f
Ali-Aref1 <ali14915@gmail.com>
2024-04-12 23:07:14 +0200

    made the placeholder clickable for no reason

--- src/components/Home/Test.tsx
+++ src/components/Home/Test.tsx
@@ -3,7 +3,7 @@ import './styles/Test.css'
 export const Test = () => {
   return (
     <div id="flex">
-      <div id="circle"><img id="testimg" src={Logo}></img></div>
+      <div id="circle" onClick={()=>{window.location.href='https://github.com/IEEE-AAST-Alexandria-Student-Branch/Frontend-2024';}}><img id="testimg" src={Logo}></img></div>
       This is our test page; To be replaced with the actual home page.<br></br>
       For now, enjoy this spinning IEEE thing.
     </div>
