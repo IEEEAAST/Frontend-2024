465850920726f0ed1aa27f2c9588d9963ac073a9
Ali-Aref1 <ali14915@gmail.com>
2024-04-12 23:21:18 +0200

    minor change

--- src/components/Home/Test.tsx
+++ src/components/Home/Test.tsx
@@ -2,8 +2,9 @@ import Logo from '../../assets/IEEELogoWhite.png'
 import './styles/Test.css'
 export const Test = () => {
   return (
-    <div id="flex">
-      <div id="circle" onClick={()=>{window.location.href='https://github.com/IEEE-AAST-Alexandria-Student-Branch/Frontend-2024';}}><img id="testimg" src={Logo}></img></div>
+    <div id="flex"><a href="https://github.com/IEEE-AAST-Alexandria-Student-Branch/Frontend-2024">
+      <div id="circle"><img id="testimg" src={Logo}></img></div>
+      </a>
       This is our test page; To be replaced with the actual home page.<br></br>
       For now, enjoy this spinning IEEE thing.
     </div>
