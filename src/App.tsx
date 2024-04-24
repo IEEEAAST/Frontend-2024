7b3cb3ec897a47de03688f341858962c8deb4685
Ali-Aref1 <ali14915@gmail.com>
2024-04-24 13:35:23 +0200

    Added empty event details page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
-import { Home } from "./pages/Home";
+import { Home } from "./pages/Home.tsx";
+import { EventDetails } from "./pages/EventDetails.tsx";
 import "./App.css"; // Import CSS file
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/event" element={<EventDetails/>}/>
     </Routes>
   );
 }
---
+++ src/pages/EventDetails.tsx
@@ -0,0 +1,5 @@
+export const EventDetails = () => {
+  return (
+    <>Event</>
+  )
+}
