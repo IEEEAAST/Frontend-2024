c3679b102648185b7ccd5566e9fcccb63a400ea8
Ali-Aref1 <ali14915@gmail.com>
2024-04-26 02:05:30 +0300

    Added navigator to our placeholder homepage

--- src/components/Home/Test.tsx
+++ src/components/Home/Test.tsx
@@ -1,6 +1,8 @@
 import Logo from '../../assets/IEEELogoWhite.png'
 import './styles/Test.css'
+import { useState } from 'react';
 export const Test = () => {
+  const [url, setUrl] = useState('');
   return (
     <div id="flex">
       <a href="https://github.com/IEEE-AAST-Alexandria-Student-Branch/Frontend-2024" target="_blank">
@@ -8,6 +10,8 @@ export const Test = () => {
       </a>
       This is our test page; To be replaced with the actual home page.<br></br>
       For now, enjoy this spinning IEEE thing.
+      <input type='text' placeholder='Enter URL path here...' style={{width:"400px",height:"40px",borderRadius:"40px",padding:"15px",color:"black"}} onChange={event=>{setUrl(event.target.value);}}></input>
+      <button className='defaultButton' onClick={()=>{window.open(`/${url}}`)}}>Go</button>
     </div>
   )
 }
