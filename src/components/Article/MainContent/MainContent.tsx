fa867d1909a3cbba936b4c77b7450d34660bad2f
Mariam Alaa <mariam.alaafathy@gmail.com>
2024-04-26 18:04:52 +0300

    Header and Footer Complete

---
+++ src/components/Article/MainContent/MainContent.tsx
@@ -0,0 +1,7 @@
+import "./styles/MainContent.css";
+
+export const MainContent = () => {
+    return <div className="main-content">
+        <span><b>Onboarding - Making minimal fun ;)</b></span>
+  </div>;
+};
---
+++ src/pages/Article.tsx
@@ -0,0 +1,13 @@
+import { Header } from "../components/Article/Header/Header.tsx";
+import { Footer } from "../components/Article/Footer/Footer.tsx";
+import { MainContent } from "../components/Article/MainContent/MainContent.tsx";
+
+export const Article = () => {
+  return (
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
+  );
+};
