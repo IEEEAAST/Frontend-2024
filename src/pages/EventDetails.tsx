161b3b1ebac7e695ed0591b14be236a9b987431c
Ali-Aref1 <ali14915@gmail.com>
2024-04-26 22:27:21 +0300

    Added icons to represent tabs on mobile scren sizes Improved responsiveness on event details page

--- src/pages/EventDetails.tsx
+++ src/pages/EventDetails.tsx
@@ -5,6 +5,12 @@ import { Tabs, TabList, TabPanels, Tab, TabPanel, TabIndicator } from '@chakra-u
 
 import Bell from '../assets/notification-bell-white.png';
 import Star from '../assets/fav-event-star-white.png';
+import ScheduleIcon from '../assets/schedule.png';
+import SpeakersIcon from '../assets/speakers.png'
+import SponsorsIcon from '../assets/sponsorships.png'
+import ResourcesIcon from '../assets/resources.png'
+import GalleryIcon from '../assets/gallery.png'
+
 
 
 export const EventDetails = () => {
@@ -22,27 +28,35 @@ export const EventDetails = () => {
           <span>Seats: <b>Limited (25 left)</b></span>
         </div>
       </div>
-      <Tabs position='relative' variant='unstyled' style={{margin:"60px 0px"}}>
-        <TabList bg={"#151F33"} style={{borderRadius:"60px",height:"60px",border:"none", padding:"2px 25px"}}>
-          <Tab>Schedule</Tab>
-          <Tab>Speakers</Tab>
-          <Tab>Sponsors</Tab>
-          <Tab>Resources</Tab>
-          <Tab className="mr-20">Gallery</Tab>
-          <button className="iconButton" style={{marginLeft:"auto", backgroundImage:`url(${Bell})`}}></button>
+      <Tabs variant='unstyled' style={{margin:"60px 0px"}}>
+        <TabList bg={"#151F33"} style={{alignItems:"center",borderRadius:"60px",height:"60px",border:"none", padding:"2px 25px"}}>
+        <Tab><span className="tabLabel">Schedule</span><span className="tabIcon"><img src={ScheduleIcon}/></span></Tab>
+        <Tab><span className="tabLabel">Speakers</span><span className="tabIcon"><img src={SpeakersIcon}/></span></Tab>
+        <Tab><span className="tabLabel">Sponsors</span><span className="tabIcon"><img src={SponsorsIcon}/></span></Tab>
+        <Tab><span className="tabLabel">Resources</span><span className="tabIcon"><img src={ResourcesIcon} /></span></Tab>
+        <Tab><span className="tabLabel">Gallery</span><span className="tabIcon"><img src={GalleryIcon} /></span></Tab>
+          <div className="iconButtonsWrapper">
+          <button className="iconButton" style={{backgroundImage:`url(${Bell})`}}></button>
           <button className="iconButton" style={{backgroundImage:`url(${Star})`}}></button>
+          </div>
           <button className="defaultButton" style={{alignSelf:"center"}}>Attend</button>
         </TabList>
         <TabIndicator mt='-1.5px' height='2px' bg='white' borderRadius='1px' />
         <TabPanels>
           <TabPanel>
-            <p>one!</p>
+            <p className="text-3xl">Schedule</p>
           </TabPanel>
           <TabPanel>
-            <p>two!</p>
+            <p className="text-3xl">Speakers</p>
           </TabPanel>
           <TabPanel>
-            <p>three!</p>
+            <p className="text-3xl">Sponsors</p>
+          </TabPanel>
+          <TabPanel>
+            <p className="text-3xl">Resources</p>
+          </TabPanel>
+          <TabPanel>
+            <p className="text-3xl">Gallery</p>
           </TabPanel>
         </TabPanels>
       </Tabs>
