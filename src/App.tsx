9b7dbae5e0cb8280afab68c17625575b4d995c1e
Yasmine El-Manzalawy <131477652+Yasmine330@users.noreply.github.com>
2024-05-05 19:25:41 +0200

    Added Sign up page

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Home } from "./pages/Home";
 import "./App.css"; // Import CSS file
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from './theme'
+import { SignUp } from "./pages/Signup";
 
 function App() {
   return (
     <ChakraProvider disableGlobalStyle={true} theme={theme}>
       <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/Signup" element={<SignUp />} />
     </Routes>
     </ChakraProvider>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
 import { NavBar } from "../components/common/navbar.tsx";
+import { SignUp } from "../pages/Signup";
 // import { Test } from "../components/Home/Test.tsx";
 export const Home = () => {
   return (
     <div>
       <NavBar/>
+      <SignUp />
       {/* <Test/> */}
     </div>
   );
