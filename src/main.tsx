09080c3d242f6a3df1d634da5041affd03606e5c
Youmna ^_^ <megaengineer2023@gmail.com>
2024-06-24 02:31:02 +0300

    Added chakra

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
 import { ColorModeScript } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 
 import theme from "./theme";
 
@@ -10,7 +11,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <App />
+      <App />{" "}
     </BrowserRouter>
   </React.StrictMode>
 );
