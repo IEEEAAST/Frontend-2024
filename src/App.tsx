e2684064825e2fb93fd99f23e3eda41f758bd8c4
Ali-Aref1 <ali14915@gmail.com>
2024-06-25 02:23:41 +0300

    added nav for dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -29,12 +29,14 @@ function App() {
     </Routes>
     <div className="fixed bottom-0 w-full h-20 flex items-center gap-5 p-5" style={{backgroundColor:"#00091a", boxShadow:"0px -2px 7px black"}}>
       <span>Navigation:</span>
-      <button className="defaultButton" onClick={()=>{window.open("/","_self")}}>Home</button>
       <button className="defaultButton" onClick= {() => getDocument("events","0HCFKfeAsaD6VjOQA7Vq").then(data =>{
         console.log(data.result?.data());
       })}>Test API</button>
+      <button className="defaultButton" onClick={()=>{window.open("/","_self")}}>Home</button>
+      <button className="defaultButton"onClick={()=>{window.open("/article/ArticleName","_self")}}>Dashboard</button>
       <button className="defaultButton"onClick={()=>{window.open("/event/Leading Your Career","_self")}}>Event</button>
       <button className="defaultButton"onClick={()=>{window.open("/article/ArticleName","_self")}}>Article</button>
+      
     </div>
     </ChakraProvider>
   );
