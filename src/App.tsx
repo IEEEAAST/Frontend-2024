daf84c83b8d9d3b068875e3c66aae6915fbc0310
Ali-Aref1 <ali14915@gmail.com>
2024-06-25 01:46:35 +0300

    fixed it!!!!!!!!!!!!!!

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {Home} from "./pages/Home";
 import "./App.css"; // Import CSS file
 import { ChakraProvider } from "@chakra-ui/react";
 import { LangProvider } from './components/common/LangContext';
+import getDocument from "./firebase/getData";
 
 import theme from "./theme";
 
@@ -17,9 +18,9 @@ function App() {
     <div className="fixed bottom-0 w-full h-20 flex items-center gap-5 p-5" style={{backgroundColor:"#00091a", boxShadow:"0px -2px 7px black"}}>
       <span>Navigation:</span>
       <button className="defaultButton" onClick={()=>{window.open("/","_self")}}>Home</button>
-      {/*<button className="defaultButton" onClick= {() => getDocument("events","0HCFKfeAsaD6VjOQA7Vq").then(data =>{
+      <button className="defaultButton" onClick= {() => getDocument("events","0HCFKfeAsaD6VjOQA7Vq").then(data =>{
         console.log(data.result?.data());
-      })}>Test API</button>*/}
+      })}>Test API</button>
       <button className="defaultButton"onClick={()=>{window.open("/event/Leading Your Career","_self")}}>Event</button>
       <button className="defaultButton"onClick={()=>{window.open("/article/ArticleName","_self")}}>Article</button>
     </div>
