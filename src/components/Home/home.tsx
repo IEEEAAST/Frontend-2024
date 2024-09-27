6c538e6731e8e789718400432e3c122fae246ac1
Ali-Aref1 <ali14915@gmail.com>
2024-09-28 02:35:35 +0300

    fixed screen size issue

--- src/components/Home/home.tsx
+++ src/components/Home/home.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef, useContext } from "react";
 import { useNavigate } from "react-router-dom";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { UserContext } from "../../App";
 import { AppConfigContext } from "../../App";
 import { RecruitmentCard } from "./RecruitmentCard";
@@ -40,11 +40,11 @@ export const HomeComp = () => {
     }, [email]);
 
     return (
-        <div className={`flex flex-col bg-cover bg-center w-full h-screen bg-homeImage`}>
+        <div className="flex flex-col bg-cover bg-center w-full h-screen bg-homeImage overflow-hidden">
             <div className="absolute w-full h-screen bg-gradient-to-t from-[#000B21]/90 via-transparent to-[#000B21]/90"></div>
             <div className="absolute w-full h-screen bg-gradient-to-r from-[#000B21]/90 via-transparent to-[#000B21]/90"></div>
-            <div className={`flex flex-col justify-center w-full h-full ${appConfig.recruiting? 'mt-36':'mt-14'}`}>
-                <div className="flex flex-col justify-start mx-20 w-full container z-10">
+            <div className={`flex flex-col justify-center w-full h-full ${appConfig.recruiting ? 'mt-36' : 'mt-14'}`}>
+                <div className="flex flex-col justify-start mx-4 md:mx-20 w-full container z-10">
                     <div className="xl:text-4xl md:text-xl sm:text-sm font-bold">
                         <p>Fostering innovation through education</p>
                         <p>technology, and professional</p>
@@ -57,7 +57,7 @@ export const HomeComp = () => {
                         </div>
                     ) : (
                         <>
-                            <div className="flex mt-4 p-2 bg-white rounded-3xl w-[60vw] sm:w-96">
+                            <div className="flex mt-4 p-2 bg-white rounded-3xl w-full max-w-md">
                                 <input
                                     ref={inputRef}
                                     className="w-4/6 text-black placeholder-black pl-2 text-sm focus:outline-none bg-transparent"
@@ -81,7 +81,7 @@ export const HomeComp = () => {
                             </div>
                         </>
                     )}
-                    <RecruitmentCard/>
+                    <RecruitmentCard />
                 </div>
             </div>
             <div className="flex flex-col items-center pb-4 z-10">
