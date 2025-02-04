ea38ce7e395b69d9dac6db5b6e5aeddfc3e188f2
MariamAlaaFathy <mariam.alaafathy@gmail.com>
2025-02-04 15:12:44 +0200

    carousel update

--- src/components/Home/Carousel.tsx
+++ src/components/Home/Carousel.tsx
@@ -4,8 +4,9 @@ import Arrow from "../../assets/carousel-arrow.jpg";
 import getCollection from "../../firebase/getCollection";
 import Award from "../../interfaces/Award";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay } from "swiper/modules";
+import { Autoplay, Pagination } from "swiper/modules"; // Add Pagination module
 import "swiper/css";
+import "swiper/css/pagination"; // Import pagination styles
 import "./styles/SwipeCarousel.css";
 
 export const SwipeCarousel = () => {
@@ -26,19 +27,23 @@ export const SwipeCarousel = () => {
   return (
     <div className="relative rounded-2xl h-full">
       <Swiper
-        modules={[Autoplay]}
+        modules={[Autoplay, Pagination]}
         spaceBetween={30}
-        slidesPerView={1}
+        slidesPerView="3"
         loop={true}
+        pagination={{
+          clickable: true,
+          dynamicBullets: true,
+        }}
         autoplay={{
           delay: 10000,
           disableOnInteraction: false,
         }}
-        style={{ height: "400px", width: "550px" }}
+        style={{ height: "400px" }}
         onSwiper={(swiper) => (swiperRef.current = swiper)}
       >
         {awards.map((award, idx) => (
-          <SwiperSlide key={idx} className="swiper-slide">
+          <SwiperSlide key={idx}>
             <div className="relative w-full h-full flex flex-col items-center justify-center p-8 bg-white border-2 border-purple-400 rounded-lg swirly-background">
               <div className="text-[20px] font-semibold text-center text-black mt-4 w-[350px] min-h-[60px]">
                 {award.name}
@@ -53,12 +58,6 @@ export const SwipeCarousel = () => {
               <div className="absolute bottom-4 right-4 text-4xl font-bold text-[#5D17EB]">
                 {award.year}
               </div>
-              <img
-                src={Arrow}
-                className="absolute right-0 top-1.5/3 transform -translate-y-1/2 w-24 h-19 cursor-pointer"
-                alt="Arrow"
-                onClick={() => swiperRef.current?.slideNext()}
-              />
             </div>
           </SwiperSlide>
         ))}
