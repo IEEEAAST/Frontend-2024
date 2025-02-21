fbc289c93c38b7acda14f4c14d242cfd13149bdd
Danial Ghobrial <danielghobrial11@gmail.com>
2025-02-21 06:55:10 +0200

    just an indent

--- src/hooks/useHorizontalScroll.tsx
+++ src/hooks/useHorizontalScroll.tsx
@@ -1,29 +1,29 @@
 import { useEffect } from 'react';
 
 const useHorizontalScroll = (containerRef: React.MutableRefObject<HTMLDivElement | null>) => {
-    useEffect(() => {
-      const container = containerRef.current;
-  
-      if (container) {
-        const handleWheelScroll = (event: WheelEvent) => {
-          const delta = Math.sign(event.deltaY);
-  
-          // Check if the content inside the container exceeds its visible area
-          const canScrollHorizontally = container.scrollWidth > container.clientWidth;
-  
-          if (canScrollHorizontally) {
-            container.scrollLeft += delta * 30;
-            event.preventDefault();
-          }
-        };
-  
-        container.addEventListener('wheel', handleWheelScroll);
-  
-        return () => {
-          container.removeEventListener('wheel', handleWheelScroll);
-        };
-      }
-    }, [containerRef]);
-  };
+  useEffect(() => {
+    const container = containerRef.current;
+
+    if (container) {
+      const handleWheelScroll = (event: WheelEvent) => {
+        const delta = Math.sign(event.deltaY);
+
+        // Check if the content inside the container exceeds its visible area
+        const canScrollHorizontally = container.scrollWidth > container.clientWidth;
+
+        if (canScrollHorizontally) {
+          container.scrollLeft += delta * 30;
+          event.preventDefault();
+        }
+      };
+
+      container.addEventListener('wheel', handleWheelScroll);
+
+      return () => {
+        container.removeEventListener('wheel', handleWheelScroll);
+      };
+    }
+  }, [containerRef]);
+};
 
 export default useHorizontalScroll;
