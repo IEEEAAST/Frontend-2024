af18095ac091897b810c4a134fd02caf2f22e15c
Ali-Aref1 <ali14915@gmail.com>
2025-03-03 18:57:48 +0200

    minor css fix

--- src/components/common/EventCard.tsx
+++ src/components/common/EventCard.tsx
@@ -79,7 +79,7 @@ export const EventCard = ({ event, color, className }: EventCardProps) => {
               className={`absolute w-full h-full backface-hidden px-[15px] ${'pt-[4px] sm:pt-[150px] md:pt-[160px] lg:pt-[190px] xl:pt-[13vw] xl:px-10'} flex flex-col sm:justify-between`}
               style={{ backfaceVisibility: 'hidden' }}
           >
-              <h3 className={`font-bold w-full text-center ${'text-[23px] sm:text-[18px] md:text-[20px] lg:text-[23px] xl:text-[2vw] sm:flex sm:justify-center'}`}>{event.title}</h3>
+              <h3 className={`font-bold w-full sm:text-center ${'text-[23px] sm:text-[18px] md:text-[20px] lg:text-[23px] xl:text-[2vw] sm:flex sm:justify-center'}`}>{event.title}</h3>
               <div className='flex gap-1 sm:self-center sm:flex-col sm:py-1'>
                   <p className={`${'font-thin text-[14px] sm:text-[13px] md:text-[14px] lg:text-[16px] xl:text-[17px] sm:flex sm:justify-center'}`}>
                       {event.type}<span className='sm:hidden'> •</span>
