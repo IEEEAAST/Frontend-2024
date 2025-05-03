fb9f2c128cfd612b94d3d8acde12d02972e663a5
Ali-Aref1 <ali14915@gmail.com>
2025-05-03 21:28:21 +0300

    improvements to follow button

--- src/components/common/FollowButton.tsx
+++ src/components/common/FollowButton.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { auth } from '../../firebase/config';
 import { toggleFollow } from '../../utils';
+import { Tooltip } from '@chakra-ui/react';
 
 interface FollowButtonProps {
   targetUserData: any;
@@ -44,20 +45,24 @@ export const FollowButton: React.FC<FollowButtonProps> = ({
     }
   };
 
-  return (
-    <button
-      className={`bg-white text-black text-[12pt] font-semibold font-[SF-Pro-Text-Medium] border-none cursor-pointer w-24 sm:w-[140px] h-[40px] rounded-full ${className}`}
-      onClick={handleFollow}
-    >
-      {canFollow ? (
-        currentUserId && targetUserData?.followers.includes(currentUserId) ? (
-          "Unfollow"
-        ) : (
-          "Follow"
-        )
-      ) : (
-        "Verify Email to Follow"
-      )}
-    </button>
+  const buttonContent=<button
+  className={`bg-white text-black text-[12pt] font-semibold font-[SF-Pro-Text-Medium] border-none cursor-pointer w-24 sm:w-[140px] h-[40px] rounded-full ${className} disabled:opacity-50 disabled:cursor-not-allowed`}
+  onClick={handleFollow}
+  disabled={!canFollow}
+>
+    {currentUserId && targetUserData?.followers.includes(currentUserId) ? (
+      "Unfollow"
+    ) : (
+      "Follow"
+    )}
+  
+</button>
+
+  return canFollow ? (
+    buttonContent
+  ) : (
+    <Tooltip label={!currentUserId? "Please sign in to follow others.":!isEmailVerified?"Please verify your email to follow others.":""} shouldWrapChildren placement="auto">
+      {buttonContent}
+    </Tooltip>
   );
 };
