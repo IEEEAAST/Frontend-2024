38b5a44e34d4cdffad07070d1766989f34286623
Ali-Aref1 <ali14915@gmail.com>
2025-05-11 23:33:38 +0300

    css fix

--- src/components/Home/About.tsx
+++ src/components/Home/About.tsx
@@ -85,7 +85,7 @@ const About = () => {
           <Card key={index} {...card} />
         ))}
       </div>
-      <div className="flex flex-col md:flex-row gap-4 px-10 mb-10">
+      <div className="flex flex-col md:flex-row gap-4 px-10 mb-10 md:px-48">
         {branchNumbers.map((number, index) => (
           <NumbersCard key={index} {...number} />
         ))}
